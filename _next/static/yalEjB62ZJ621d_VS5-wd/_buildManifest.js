323b0f13e7dcd9a99fabeac59bf959b24df73d41
Jason R <mail4jasonr@gmail.com>
2020-09-04 20:36:41 +0800

    Initial commit

---
+++ _next/static/yalEjB62ZJ621d_VS5-wd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F70a03283c9ed137ea1b8.css","static\u002Fchunks\u002Fpages\u002Findex-0a339ee2756edc413eb5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
